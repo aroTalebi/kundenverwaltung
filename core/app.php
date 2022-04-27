4e045b169393b99c24e5b4ffd31cf47acd108f89
Arash Talebi <arash8889@gmail.com>
2022-04-27 11:29:08 +0200

    Create router

---
+++ core/app.php
@@ -0,0 +1,40 @@
+<?php
+class app
+{
+    public $controller = 'index';       //Default Controller
+    public $method = 'index';           //Default Methode
+    public $params = [];                //Data to methode
+    //Router here beginn
+    function __construct()
+    {
+        if (isset($_GET['url'])) {
+            $url = $_GET['url'];
+            $url = $this->parseurl($url);
+            $this->controller = $url[0];
+            unset($url[0]);
+            if (isset($url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+            $this->params = array_values($url);
+        }
+        $controllerUrl = 'controllers/' . $this->controller . '.php';
+        if (file_exists($controllerUrl)) {
+            require($controllerUrl);
+            $object = new $this->controller;
+            $object->model($this->controller);
+            if (method_exists($object, $this->method)) {
+                $arr = [$object, $this->method];
+                call_user_func_array($arr, $this->params);
+            }
+        }
+    }
+    function parseurl($url)                             //Explode URL to controller und methode und filter
+    {
+        $url = filter_var($url, FILTER_SANITIZE_URL);   //URL should only the Address
+        $url = strip_tags($url);
+        $url = rtrim($url, '/');
+        $url = explode('/', $url);
+        return $url;
+    }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php
+require('core/app.php');
+require('core/controller.php');
+require('core/model.php');
+require('core/config.php');
+
+new app;
+?>
